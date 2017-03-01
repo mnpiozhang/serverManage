81da1ec5fca3d139dbbca60b1b40944294d67d90
cloudhzl <cloudhzl@163.com>
2017-03-01 11:41:46 +0800

    修改bug

--- web/logger.py
+++ web/logger.py
@@ -4,33 +4,6 @@ import logging
 import os
 from datetime import datetime
 
-'''
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-def set_log():
-    # return log object
-    logfile = os.path.abspath(os.path.join(BASE_DIR,'logs/dj'))+datetime.now().strftime("%Y-%m-%d")+'.log'
-    logger_local = logging.getLogger('sm')
-    
-    filehdlr = logging.FileHandler(logfile)
-    console = logging.StreamHandler()
-    
-    formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
-    filehdlr.setFormatter(formatter)
-    console.setFormatter(formatter)
-    
-    logger_local.addHandler(filehdlr)
-    logger_local.addHandler(console)
-    
-    logger_local.setLevel(logging.DEBUG)
-    
-    return logger_local
-
-
-
-log = set_log()
-'''
-
 
 THIS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOGGING = {
@@ -50,7 +23,7 @@ LOGGING = {
         'file': {
             'level': 'INFO',
             'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.abspath(os.path.join(THIS_DIR,'logs/dj.log')),
+            'filename': os.path.abspath(os.path.join(THIS_DIR,'logs/sm.log')),
             'when': 'midnight',
             'formatter': 'verbose'
         }
